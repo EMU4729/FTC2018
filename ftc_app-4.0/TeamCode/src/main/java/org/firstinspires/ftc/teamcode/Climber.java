2270cec069c6ae13b4c9dc5ecb36d69cf84af207
FRC-4 <unswfrcmedia@gmail.com>
2018-11-07 18:29:39 +1100

    trying to climber without knowing the design

---
+++ ftc_app-4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Climber.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Climber {
+    public DcMotor hook;
+    private static final double SPEED = 1;
+
+    
+}
