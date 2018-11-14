1bc39c6e829fc50e2b0e52604949088f43d8dc44
FRC-4 <unswfrcmedia@gmail.com>
2018-11-14 20:04:00 +1100

    started time base version

---
+++ ftc_app-4.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimebasedAuto.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class TimebasedAuto extends OpMode {
+    private Motors motors;
+    private ElapsedTime runtime = new ElapsedTime();
+    private Grabber grabber;
+
+    private static final double ROBOT_FIELD = 358.14; //cm
+    private static final double ROBOT_FIELD_HALF = 179.07; //cm
+    private double squareWidth = 596.9; //mm
+
+    private final double distanceThreshold = 100; //mm
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Auto ready");
+        motors = new Motors(hardwareMap);
+        grabber = new Grabber(hardwareMap);
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+        runtime.startTime();
+    }
+
+    @Override
+    public void loop() {
+        forward();
+        if (runtime.time() == 10){
+            turnLeft();
+        }
+        if (runtime.time() == 15) {
+            forward();
+        }
+    }
+
+    private void forward() {
+        motors.arcadeDrive(1, 0);
+    }
+
+    private void turnLeft() {
+        motors.arcadeDrive(0, -1);
+    }
+
+    private void turnRight() {
+        motors.arcadeDrive(0, 1);
+    }
+
+    private void stopMotors() {
+        motors.arcadeDrive(0,0);
+    }
+
+    @Override
+    public void stop() {
+        stopMotors();
+        grabber.stopSpin();
+    }
+}
